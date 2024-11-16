e3aacaa6d6a559da60a367ec944d3ad8da811798
lucas-bernardino <lucasgber@gmail.com>
2024-11-16 10:32:44 -0300

    Fix bug with missing TICKS_PER_SECOND multipler when adding aperiodic task

--- Src/main.c
+++ Src/main.c
@@ -1,14 +1,19 @@
 #include <stdint.h>
 #include "miros.h"
 
-// Task visualization variables (for debugging or visualization)
+// For debugging periodic tasks
 uint32_t task1Visualizer = 0;
 uint32_t task2Visualizer = 0;
 uint32_t task3Visualizer = 0;
 
+// For debugging periodic tasks
 uint32_t aperiodicExecution = 0;
+uint32_t aperiodicExecution2 = 0;
 
+// Shared resource variable
 int32_t resource = 0;
+
+// Variables that will be used to simulate heavy computation
 volatile int counter, j;
 
 // Stack arrays for the tasks
@@ -22,19 +27,19 @@ OSThread task1Thread;
 OSThread task2Thread;
 OSThread task3Thread;
 
-// Function prototypes for tasks
+// Prototypes
 void task1();
 void task2();
 void task3();
 void aperiodicTask();
+void aperiodicTask2();
 
+// Mutex that will be used when accessing critical section.
 semaphore mutex;
 
 int main() {
-    // Initialize the OS with idle thread stack
     OS_init(stack_idleThread, sizeof(stack_idleThread));
 
-    // Start periodic tasks
     OSThread_start(&task1Thread, 5U, &task1, stackTask1, sizeof(stackTask1),
                    3 * TICKS_PER_SEC, 5 * TICKS_PER_SEC);
 
@@ -44,20 +49,23 @@ int main() {
     OSThread_start(&task3Thread, 1U, &task3, stackTask3, sizeof(stackTask3),
                    1 * TICKS_PER_SEC, 10 * TICKS_PER_SEC);
 
-    // Add aperiodic tasks with specific arrival times and execution costs
-    addAperiodicTask(aperiodicTask, 1 * TICKS_PER_SEC, 1);  // Aperiodic Task 1 arrives at T=5 and has cost 2
+    // Aperiodic task with arrival at T = 1 and cost of C = 1
+    addAperiodicTask(aperiodicTask, 1 * TICKS_PER_SEC, 1 * TICKS_PER_SEC);
+
+    // Aperiodic task with arrival at T = 4 and cost of C = 1
+    addAperiodicTask(aperiodicTask2, 4 * TICKS_PER_SEC, 1 * TICKS_PER_SEC);
 
     sem_init(&mutex, 1);
-    // Run the OS
+
     OS_run();
 }
 
 void task1() {
     while (1) {
         task1Visualizer++;
-    	sem_wait(&mutex, &task1Thread);
+    	/*sem_wait(&mutex, &task1Thread);
         resource = resource + 5;
-    	sem_post(&mutex, &task1Thread);
+    	sem_post(&mutex, &task1Thread);*/
         TaskAction(&task1Thread, task1Thread.remainingTime);
     }
 }
@@ -72,17 +80,21 @@ void task2() {
 void task3() {
     while (1) {
         task3Visualizer++;
-    	sem_wait(&mutex, &task3Thread);
+    	/*sem_wait(&mutex, &task3Thread);
         resource = resource - 5;
-        // Simulando computacao pesada. Task 3 está tomando tempo dentro da zona critica
+        // Simulating heavy computation, where task3 is taking a lot of time...
         for (counter = 0; counter < 1000000; counter++) {
             j = counter * counter;
         }
-        sem_post(&mutex, &task3Thread);
+        sem_post(&mutex, &task3Thread);*/
     	TaskAction(&task3Thread, task3Thread.remainingTime);
     }
 }
 
 void aperiodicTask() {
 	aperiodicExecution++;
+}
+
+void aperiodicTask2() {
+	aperiodicExecution2++;
 }
